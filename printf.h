38894f5ee25ea63062997f48540363efb45520b7
ehuyberegit <ehuybere@c1r9s3.42lisboa.com>
2025-04-25 13:35:15 +0100

    first implementations

--- printf.h
+++ printf.h
@@ -1,42 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   printf.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ehuybere <ehuybere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/21 16:48:11 by ehuybere          #+#    #+#             */
-/*   Updated: 2025/04/25 13:34:41 by ehuybere         ###   ########.fr       */
+/*   Created: 2025/04/25 12:31:39 by ehuybere          #+#    #+#             */
+/*   Updated: 2025/04/25 13:23:12 by ehuybere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "printf.h"
+#ifndef PRINTF_H
+# define PRINTF_H
 
-int	ft_printf(const char *format, ...)
-{
-	int		len;
-	int		i;
-	va_list	arg_list;
-	
-	va_start(arg_list, format);
+# include <stdarg.h>
+# include "libft/libft.h"
 
-	len = 0;
-	i = 0;
-	while(format[i] != '\0')
-	{
-		if (ft_isprint(format[i]) == 1)
-		{
-			ft_putchar_fd(format[i], 1);
-			len ++;
-		}
-		i++;
-	}
-	return (len);
-}
-int	main(void)
-{
-	char	str[] = "YO Y.>/OYO";
-	
-	ft_printf(str);
-	return (0);
-}
+int	ft_printf(const char *format, ...);
+
+#endif
